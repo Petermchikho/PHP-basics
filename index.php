da2c053b067c8493f9ac64a7555d21483e6e9310
Petermchikho <petercharlesmchikho1@gmail.com>
2025-06-23 09:18:40 +0200

    PDO first steps

--- index.php
+++ index.php
@@ -1,14 +1,34 @@
 <?php
 require "functions.php";
-$uri=parse_url($_SERVER['REQUEST_URI']);
-$uri=$uri['path'];
+//require "router.php";
 
-if($uri=='/'){
-    require "controllers/index.php";
-}elseif ($uri=='/about'){
-    require "controllers/about.php";
-}elseif ($uri=='/contact'){
-    require "controllers/contact.php";
+// connect to our MySQL database
+$dsn = 'mysql:host=localhost;port=3306;dbname=myapp;charset=utf8mb4';
+$pdo=new PDO($dsn, 'root', '2001Pboy@');
+
+$statement=$pdo->prepare('select * from posts');
+
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+//dd($posts);
+
+foreach ($posts as $post) {
+    echo "<h1>{$post['title']}</h1>";
 }
 
-
+//class Person{
+//    public $name;
+//    public $age;
+//    public function breathe()
+//    {
+//        echo $this->name . ' is breathe';
+//    }
+//}
+//$person = new Person();
+//$person->name = "John Doe";
+//$person->age = 25;
+//
+////dd($person->name);
+//$person->breathe();
